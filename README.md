a3be9c6968a7aae20c9f13f1253c654e800512e7
Wojciech Ziniewicz <wojtek@gamecodehq.com>
2023-09-29 20:33:53 +0200

    feat: 🎸 Pythonize the code

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # intercity_ping
 Simple script that will save the 1.1.1.1 ping response times while traveling thru the country on the train
+
+## Installation
+
+```bash
+pip install matplotlib numpy
+```
+
+## Usage
+
+Run and record:
+
+```bash
+python script_name.py --interval 10 --timeout 10 --address 1.1.1.1
+```
+
+Plot the results:
+
+```bash
+python script_name.py --plot
+```
+
+
--- ping.py
+++ ping.py
@@ -1,47 +1,74 @@
+import argparse
 import subprocess
 import time
 import csv
 from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Set the interval X (in seconds) between pings
-X = 10
 
-# Set the timeout Y (in seconds) for each ping
-Y = 10
+class PingTester:
+    def __init__(self, interval, timeout, address, csv_file_name='ping_results.csv'):
+        self.interval = interval
+        self.timeout = timeout
+        self.address = address
+        self.csv_file_name = csv_file_name
 
-# address
-address = '1.1.1.1'
+    def run(self):
+        with open(self.csv_file_name, mode='a', newline='') as file:
+            writer = csv.writer(file)
 
-# Define the CSV file name
-csv_file_name = 'ping_results.csv'
+            if file.tell() == 0:
+                writer.writerow(['Timestamp', 'Response Time (s)'])
 
-# Open the CSV file in write mode
-with open(csv_file_name, mode='a', newline='') as file:
-    # Create a CSV writer object
-    writer = csv.writer(file)
-    
-    # Write the header row to the CSV file
-    writer.writerow(['Timestamp', 'Response Time (s)'])
-    
-    # Start the infinite loop to keep pinging at intervals of X seconds
-    while True:
-        # Get the current timestamp
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        
-        try:
-            # Execute the ping command and measure the response time
-            print("Ping")
-            start_time = time.time()
-            subprocess.check_output(['ping', '-c', '1', '-W', str(Y), address])
-            response_time = time.time() - start_time
-            print(f"Response time: #{response_time}")
-        except subprocess.CalledProcessError:
-            # If ping command fails (e.g., no internet), set response time to Y seconds
-            response_time = Y
-        
-        # Log the timestamp and response time to the CSV file
-        writer.writerow([timestamp, response_time])
-        file.flush()
-        
-        # Sleep for X seconds before the next ping
-        time.sleep(X)
+            while True:
+                timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+                try:
+                    print("Ping")
+                    start_time = time.time()
+                    subprocess.check_output(['ping', '-c', '1', '-W', str(self.timeout), self.address])
+                    response_time = time.time() - start_time
+                    print(f"Response time: {response_time}")
+                except subprocess.CalledProcessError:
+                    response_time = self.timeout
+
+                writer.writerow([timestamp, response_time])
+                file.flush()
+                time.sleep(self.interval)
+
+
+def plot_results(csv_file_name):
+    timestamps = []
+    response_times = []
+    with open(csv_file_name, mode='r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header row
+        for row in reader:
+            timestamps.append(row[0])
+            response_times.append(float(row[1]))
+
+    plt.plot(timestamps, response_times, marker='o')
+    plt.xticks(rotation=45)
+    plt.ylabel('Response Time (s)')
+    plt.title('Ping Response Times')
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Ping Tester')
+    parser.add_argument('--interval', type=int, default=10, help='Interval between pings in seconds')
+    parser.add_argument('--timeout', type=int, default=10, help='Timeout for each ping in seconds')
+    parser.add_argument('--address', type=str, default='1.1.1.1', help='Remote server address')
+    parser.add_argument('--plot', action='store_true', help='Plot results using matplotlib and numpy')
+
+    args = parser.parse_args()
+
+    tester = PingTester(args.interval, args.timeout, args.address)
+
+    if args.plot:
+        plot_results(tester.csv_file_name)
+    else:
+        tester.run()
+
